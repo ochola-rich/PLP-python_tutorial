669109446bb8a6466690d297740278c52c43151e
Reechy254 <richardochola3@gmail.com>
2024-10-31 09:30:56 +0300

    done

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name?')
+Age = input('what is your age?')
+Location = input('Where are you from?')
+
+print('Hello '+ name+', you are '+Age+'years old and you are from '+Location)
